f5ddc50608a9145aed31b1bc8592365c26421ba6
koszczi <koszczi@gmail.com>
2023-10-14 22:24:36 +0200

    new index on column date

--- src/main/java/com/koszczi/calendar/model/event/Event.java
+++ src/main/java/com/koszczi/calendar/model/event/Event.java
@@ -16,7 +16,10 @@ import java.time.temporal.WeekFields;
 import java.util.Locale;
 
 @Entity
-@Table(name = "calendar_events", indexes = { @Index(name = "event_year_week", columnList = "calendar_year, week_of_year")} )
+@Table(name = "calendar_events", indexes = {
+    @Index(name = "event_year_week", columnList = "calendar_year, week_of_year"),
+    @Index(name = "event_date", columnList = "date")
+} )
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode(of = "id")
 @Getter
